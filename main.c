87837c86e0e6afb1b23d7fc2d28906b0623280eb
Support 30MHz <support30mhz@MacBook-Air-van-Support.local>
2022-07-22 10:27:18 +0200

    Fixed the different directions images, but broken by new use of structs and gamedata

--- main.c
+++ main.c
@@ -20,118 +20,140 @@
 
 //gcc main.c MLX42/libmlx42.a -I include -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit
 
-typedef struct character
+typedef enum e_direction
 {
-	bool	up;
-	bool	down;
-	bool	right;
-	bool	left;
-}	character_t;
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+} t_direction;
 
+typedef struct s_fox
+{
+	t_direction	direction;
+	mlx_texture_t	*down_texture;
+	mlx_texture_t	*up_texture;
+	mlx_texture_t	*left_texture;
+	mlx_texture_t	*right_texture;
+	mlx_image_t *fox_img;
+}	t_fox;
+
+typedef struct s_game_data
+{
+	t_fox *main_fox;
+	mlx_t *mlx;
+}	t_game_data;
 
 static	mlx_image_t *sprite_img;
-static	mlx_image_t *fox_img;
-static	mlx_image_t *fox_up;
-static	character_t	fox;
 
+void	load_fox_textures(t_fox *fox)
+{
+	fox->down_texture = mlx_load_png("Fox_Down.png");
+	fox->up_texture = mlx_load_png("Fox_Up.png");
+	fox->left_texture = mlx_load_png("Fox_Left.png");
+	fox->right_texture = mlx_load_png("Fox_Right.png");
+}
 void	hook(void *param)
 {
+	t_game_data *game_data;
 	mlx_t	*mlx;
-	mlx_texture_t	*foxdown_texture;
-	mlx_texture_t	*foxup_texture;
-	mlx_texture_t	*foxleft_texture;
-	mlx_texture_t	*foxright_texture;
-
-	foxdown_texture = mlx_load_png("Fox_Down.png");
-	foxup_texture = mlx_load_png("Fox_Up.png");
-	foxleft_texture = mlx_load_png("Fox_Left.png");
-	foxright_texture = mlx_load_png("Fox_Right.png");
-
-	mlx = param; //mlx is nu void pointer? waarom?
-	if (mlx_is_key_down(param, MLX_KEY_ESCAPE))
-		mlx_close_window(param);
-	if (mlx_is_key_down(param, MLX_KEY_UP))
+	t_fox 	*fox;
+	game_data = param;
+	mlx = game_data->mlx; //mlx is nu void pointer? waarom?
+	fox = game_data->main_fox;
+	load_fox_textures(fox);
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
 	{
-		//mlx_delete_texture(foxdown_texture);
-			if (fox.up == 0)
+			if (fox->direction != UP)
 			{
 				uint32_t tx;
 				uint32_t ty;
-				tx = fox_img->instances->x;
-				ty = fox_img->instances->y;
-				printf("uint32 x = %u\n", tx);
-				printf("uint32 y = %u\n", ty);
-				mlx_draw_texture(fox_img, foxup_texture, tx, ty);
-				printf("Foxup = 0\n");
-				fox.down = 0;
-				printf("Foxdown = 0\n");
-				fox.up = 1;
-				printf("Foxup = 1\n");
-				fox.left = 0;
-				printf("Foxleft = 0\n");
-				fox.right = 0;
-				printf("Foxright = 0\n");
-				//fox_up = mlx_texture_to_image(mlx, foxup_texture);
-				//mlx_image_to_window(mlx, fox_up, (fox_img->instances[0].x), (fox_img->instances[0].y));
+				tx = fox->fox_img->instances->x;
+				ty = fox->fox_img->instances->y;
+				fox->direction = UP;
+				mlx_delete_image(mlx, fox->fox_img);
+				fox->fox_img = mlx_texture_to_image(mlx, fox->up_texture);
+				mlx_image_to_window(mlx, fox->fox_img, tx, ty);
 			}
 		//mlx_image_to_window(mlx, fox_img, (fox_img->instances[0].x), (fox_img->instances[0].y));
-		fox_img->instances[0].y -= 5;
+		fox->fox_img->instances[0].y -= 5;
 		//fox_img = mlx_texture_to_image(mlx, foxup_texture);
 	}
-	if (mlx_is_key_down(param, MLX_KEY_DOWN))
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
 	{
-		if (fox.down == 0)
+		if (fox->direction != DOWN)
 			{
-				fox.down = 1;
-				fox.up = 0;
-				fox.left = 0;
-				fox.right = 0;
-				//mlx_draw_texture(fox_img, foxdown_texture, (fox_img->instances[0].x), (fox_img->instances[0].y));
-				//fox_up = mlx_texture_to_image(mlx, foxup_texture);
-				//mlx_image_to_window(mlx, fox_up, (fox_img->instances[0].x), (fox_img->instances[0].y));
+				uint32_t tx;
+				uint32_t ty;
+				tx = fox->fox_img->instances->x;
+				ty = fox->fox_img->instances->y;
+				fox->direction = DOWN;
+				mlx_delete_image(mlx, fox->fox_img);
+				fox->fox_img = mlx_texture_to_image(mlx, fox->down_texture);
+				mlx_image_to_window(mlx, fox->fox_img, tx, ty);
 			}
-		fox_img->instances[0].y += 5;
+		fox->fox_img->instances[0].y += 5;
 
 	}
-	if (mlx_is_key_down(param, MLX_KEY_LEFT))
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
 	{
-		mlx_draw_texture(sprite_img, foxup_texture, 0, 0);
-		fox_img->instances[0].x -= 5;
+		if (fox->direction != LEFT)
+		{
+			uint32_t tx;
+			uint32_t ty;
+			tx = fox->fox_img->instances->x;
+			ty = fox->fox_img->instances->y;
+			fox->direction = LEFT;
+			mlx_delete_image(mlx, fox->fox_img);
+			fox->fox_img = mlx_texture_to_image(mlx, fox->left_texture);
+			mlx_image_to_window(mlx, fox->fox_img, tx, ty);
+		}
+		fox->fox_img->instances[0].x -= 5;
 		//mlx_draw_texture(sprite_img, foxdown_texture, 0, 0);
 	}
-	if (mlx_is_key_down(param, MLX_KEY_RIGHT))
-		fox_img->instances[0].x += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+	{
+		if (fox->direction != RIGHT)
+		{
+			uint32_t tx;
+			uint32_t ty;
+			fox->direction = RIGHT;
+			tx = fox->fox_img->instances->x;
+			ty = fox->fox_img->instances->y;
+			mlx_delete_image(mlx, fox->fox_img);
+			fox->fox_img = mlx_texture_to_image(mlx, fox->right_texture);
+			mlx_image_to_window(mlx, fox->fox_img, tx, ty);
+		}
+		fox->fox_img->instances[0].x += 5;
+	}
 }
 
 //int32_t is the C standard for an int with a specified size of 32 bits (int varies between 16 & 32)
 int32_t main(void)
 {
-	mlx_t	*mlx;
-	mlx_texture_t	*foxdown_texture;
+	t_game_data game_data;
+
 	
-	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
-	if (!mlx)
+	game_data.mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!game_data.mlx)
 		exit(EXIT_FAILURE); //These EXIT statuses failure is 1 succes = 0 are define in stdlib.h
 	
-	foxdown_texture = mlx_load_png("Fox_Down.png");
-	fox_img = mlx_new_image(mlx, 128, 128);
-	fox.down = 1;
-	fox.up = 0;
-	fox.left = 0;
-	fox.right = 0;
-	memset(fox_img->pixels, 255, fox_img->width * fox_img->height * BPP);
-	mlx_image_to_window(mlx, fox_img, 0, 0);
+	game_data.main_fox->fox_img = mlx_new_image(game_data.mlx, 128, 128);
+	memset(game_data.main_fox->fox_img->pixels, 255, game_data.main_fox->fox_img->width * game_data.main_fox->fox_img->height * BPP);
+	mlx_image_to_window(game_data.mlx, game_data.main_fox->fox_img, 0, 0);
 	//mlx_draw_texture(fox_img, foxdown_texture, 0, 0);
 	
-	sprite_img = mlx_new_image(mlx, 128, 128); //Creates a new image
-	memset(sprite_img->pixels, 255, sprite_img->width * sprite_img->height * BPP);//set the channels of each pixel in the image to the maximum byte value of 255
-	mlx_image_to_window(mlx, sprite_img, 0, 0); //Adds an image to the render queue
+	//sprite_img = mlx_new_image(game_data.mlx, 128, 128); //Creates a new image
+	//memset(sprite_img->pixels, 255, sprite_img->width * sprite_img->height * BPP);//set the channels of each pixel in the image to the maximum byte value of 255
+	//mlx_image_to_window(game_data.mlx, sprite_img, 0, 0); //Adds an image to the render queue
 	//mlx_put_pixel(sprite_img, 64, 64, 0xeb4034FF); //Single red pixel in the middle of image
 	
-	mlx_loop_hook(mlx, &hook, mlx);
-	mlx_loop(mlx); //Rendering of mlx, which loops until the window is closed
-	mlx_delete_image(mlx, fox_img); //Cleanup the image after exit
-	mlx_terminate(mlx);
+	mlx_loop_hook(game_data.mlx, &hook, &game_data);
+	mlx_loop(game_data.mlx); //Rendering of mlx, which loops until the window is closed
+	mlx_delete_image(game_data.mlx, game_data.main_fox->fox_img); //Cleanup the image after exit
+	mlx_terminate(game_data.mlx);
 	return (EXIT_SUCCESS);
 }
 
