8a43cd0892e56c9841cadc492126bfaf3ba5a85b
Sharon@Codam <sharonvrielink@gmail.com>
2022-09-08 19:20:28 +0200

    Started on map_reader

--- src/map_reader.c
+++ src/map_reader.c
@@ -6,7 +6,7 @@
 /*   By: svrielin <svrielin@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/08 18:10:25 by svrielin      #+#    #+#                 */
-/*   Updated: 2022/09/08 18:13:36 by svrielin      ########   odam.nl         */
+/*   Updated: 2022/09/08 19:18:44 by svrielin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void	read_map(const char *map_file)
 {
-	int	fd;
+	int		fd;
+	t_list	*map_list;
 
 	fd = open(map_file, O_RDONLY);
 	if (!fd)
 		return (NULL);
+	map_list = ft_lstnew(get_next_line(fd));
 	
 }
