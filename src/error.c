20bf0cf7f4265fafd281201d8fcce9c683d43262
Sharon@Codam <sharonvrielink@gmail.com>
2022-09-22 16:38:36 +0200

    Added ft_printf to libft

--- src/error.c
+++ src/error.c
@@ -6,18 +6,25 @@
 /*   By: svrielin <svrielin@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/22 14:18:03 by svrielin      #+#    #+#                 */
-/*   Updated: 2022/09/22 15:29:10 by svrielin      ########   odam.nl         */
+/*   Updated: 2022/09/22 16:38:31 by svrielin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 
+char *error_messages(t_error errorcode)
+{
+	char	*message[] = {
+		"Exit: Succes!\n",
+		"Error: map unvalid, not surrounded by walls\n",
+		"Error: map unvalid, unvalid char found\n",
+	};
+	return (message[errorcode]);
+}
+
 int	so_long_error(t_error errorcode)
 {
-	if (errorcode == MAP_WALLS)
-		printf("Error: map unvalid, not surrounded by walls\n");
-	if (errorcode == MAP_CHAR)
-		printf("Error: map unvalid, unvalid char found\n");
+	ft_putstr_fd(error_messages(errorcode), 1);
 	// delete_img(game->mlx, &game->sprite);
 	// mlx_terminate(game->mlx);
 	exit(EXIT_FAILURE);
