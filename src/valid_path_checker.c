21fff79c5d420a46928b5f3ec55f02dcf5e7e294
Sharon@Codam <sharonvrielink@gmail.com>
2022-09-24 15:46:37 +0200

    Started valid path check

--- src/valid_path_checker.c
+++ src/valid_path_checker.c
@@ -6,7 +6,15 @@
 /*   By: svrielin <svrielin@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/22 19:02:05 by svrielin      #+#    #+#                 */
-/*   Updated: 2022/09/22 19:02:06 by svrielin      ########   odam.nl         */
+/*   Updated: 2022/09/24 15:38:33 by svrielin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <so_long.h>
+
+bool check_valid_path(t_map *map)
+{
+	char **copy_map_grid;
+
+	
+}
